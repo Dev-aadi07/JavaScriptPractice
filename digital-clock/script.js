1eeebd49fe1cae08b0260b9f8ff07f48089a2f12
Adarsh <aadarshjha07137@gmail.com>
2026-07-12 17:23:10 +0530

    digital-clock complete

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -2,6 +2,7 @@ const clck = document.querySelector(".clock");
 const dayElement = document.querySelector(".day");
 const dinank = document.querySelector(".date");
 const toggleBtn = document.querySelector(".toggle");
+const greetingElement = document.querySelector(".greet");
 let is24Hour = false;
 
 const days = [
@@ -14,7 +15,7 @@ const days = [
     "Saturday"
   ];
 
-  const months = [
+const months = [
     "January",
     "February",
     "March",
@@ -29,8 +30,33 @@ const days = [
     "December"
   ];
 
+const greet = [
+  "Good Morning ☀️",
+  "Good Afternoon 🌤️",
+  "Good Evening 🌆",
+  "Good Night 🌙"
+];
+
 function updateTime() {
   const now = new Date();
+  const currentHour = now.getHours();
+  if(currentHour < 12) {
+    greetingElement.textContent = greet[0];
+    document.body.style.backgroundColor = "Light Yellow";
+    greetingElement.style.backgroundColor = "Light Yellow";
+  } else if(currentHour < 17) {
+    greetingElement.textContent = greet[1];
+    document.body.style.backgroundColor = "Sky Blue";
+    greetingElement.style.backgroundColor = "Sky Blue";
+  } else if(currentHour < 21) {
+    greetingElement.textContent = greet[2];
+    document.body.style.backgroundColor = "Orange";
+    greetingElement.style.backgroundColor = "Orange";
+  } else {
+    document.body.style.backgroundColor = "Dark Blue";
+    greetingElement.textContent = greet[3];
+    greetingElement.style.backgroundColor = "Dark Blue";
+  }
 
   let hours = String(now.getHours()).padStart(2, "0");
   let minutes = String(now.getMinutes()).padStart(2, "0");
@@ -41,14 +67,16 @@ function updateTime() {
   let month = now.getMonth();
   let year = now.getFullYear();
   
+  const separator = now.getSeconds() % 2 === 0 ? ":" : " ";
+
   if (is24Hour) {
-    clck.textContent = `${hours}:${minutes}:${seconds}`;
+    clck.textContent = `${hours}${separator}${minutes}${separator}${seconds}`;
   }
   else {
     const amPm = hours>=12 ? "PM" : "AM";
     hours = hours%12;
     hours = hours === 0 ? 12 : hours;
-    clck.textContent = `${hours}:${minutes}:${seconds} ${amPm}`;
+    clck.textContent = `${hours}${separator}${minutes}${separator}${seconds} ${amPm}`;
   }
 
   dayElement.textContent = days[day];
