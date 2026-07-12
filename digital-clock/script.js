68dc6d56b3a9422b0436883e95af60f75a746b71
Adarsh <aadarshjha07137@gmail.com>
2026-07-12 16:44:10 +0530

    add 24h format toggle

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,6 +1,8 @@
 const clck = document.querySelector(".clock");
 const dayElement = document.querySelector(".day");
 const dinank = document.querySelector(".date");
+const toggleBtn = document.querySelector(".toggle");
+let is24Hour = false;
 
 const days = [
     "Sunday",
@@ -30,28 +32,40 @@ const days = [
 function updateTime() {
   const now = new Date();
 
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
+  let hours = String(now.getHours()).padStart(2, "0");
+  let minutes = String(now.getMinutes()).padStart(2, "0");
+  let seconds = String(now.getSeconds()).padStart(2, "0");
 
   let day = now.getDay();
   let date = now.getDate();
   let month = now.getMonth();
   let year = now.getFullYear();
+  
+  if (is24Hour) {
+    clck.textContent = `${hours}:${minutes}:${seconds}`;
+  }
+  else {
+    const amPm = hours>=12 ? "PM" : "AM";
+    hours = hours%12;
+    hours = hours === 0 ? 12 : hours;
+    clck.textContent = `${hours}:${minutes}:${seconds} ${amPm}`;
+  }
 
-  const amPm = hours>=12 ? "PM" : "AM";
-
-  hours = hours%12;
-  hours = hours === 0 ? 12 : hours;
-
-  hours = String(hours).padStart(2, "0");
-  minutes = String(minutes).padStart(2, "0");
-  seconds = String(seconds).padStart(2, "0");
-
-  clck.textContent = `${hours}:${minutes}:${seconds} ${amPm}`;
   dayElement.textContent = days[day];
   dinank.textContent = `${date} ${months[month]} ${year}`;
 }
+
+toggleBtn.addEventListener("click", () => {
+  is24Hour = !is24Hour;
+
+  if (is24Hour) {
+    toggleBtn.textContent = "12H";
+  } else {
+    toggleBtn.textContent = "24H";
+  }
+
+  updateTime();
+});
 
 updateTime();
 setInterval(updateTime, 1000);
